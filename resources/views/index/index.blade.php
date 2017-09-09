eebe241b0c99a4b1de4f9d599a0fffedb9659bd7
hold766774 <1106246044@qq.com>
2017-09-09 15:17:41 +0000

    layout done

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.main')
+@section('title')
+    首页
+@endsection
+@section('content')
+    <div class="row col-md-6 col-md-offset-3">
+        <div class="panel panel-primary panel_index">
+            <div class="panel-heading">
+                <h3 class="panel-title">欢迎来到SampleApp</h3>
+            </div>
+            <div class="panel-body">
+                如果你还没有账号，请点击<a href="#">注册</a>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/layout/_header.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar  navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+
+            <a class="navbar-brand" href="#">SampleApp</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">用户列表</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">用户名 <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">个人中心</a></li>
+                        <li><a href="#">编辑资料</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#"><button class="btn btn-danger col-md-10 col-md-offset-1">退出</button></a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
